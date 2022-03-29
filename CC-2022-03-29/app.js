809f21fa1099209906e33d2601dd3a246d717c04
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-03-29 19:03:30 -0400

    Grasshopper - Messi Goals

---
+++ CC-2022-03-29/app.js
@@ -0,0 +1,15 @@
+/* 
+Competition	        Goals
+La Liga 	        43
+Champions League	10
+Copa del Rey	    5
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year. */ 
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
