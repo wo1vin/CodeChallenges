13380c4c50be4f6c4deae3f27a170c9f0420e2de
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-03-30 19:14:32 -0400

    Will there be enough space?

---
+++ CC-2022-03-30/app.js
@@ -0,0 +1,10 @@
+/* You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take. */
+function enough(cap, on, wait) {
+    //if there is space, return 0. else, return # that dont fit
+    return on + wait < cap? 0 : Math.abs(cap - on - wait);  
+  }
