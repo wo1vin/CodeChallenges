36a65f0a6d0d91c978329910a686cd8528b23ab6
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-04-03 20:43:50 -0400

    Expressions Matter

---
+++ CC-2022-04-03/app.js
@@ -0,0 +1,8 @@
+/* Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained */
+function expressionMatter(a, b, c) {
+    return Math.max(a + b + c, 
+                    a * b * c, 
+                   (a + b) * c,
+                    a * (b + c));
+    }
