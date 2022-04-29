d4cdd3ba9d81cb13f913844fd01abe6f1bd2a12b
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-04-29 11:21:23 -0400

    Welcome

---
+++ CC-2022-04-29/app.js
@@ -0,0 +1,31 @@
+/* Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input. */
+function greet(language){
+    const translator = {
+      english: 'Welcome',
+      czech: 'Vitejte',
+      danish: 'Velkomst',
+      dutch: 'Welkom',
+      estonian: 'Tere tulemast',
+      finnish: 'Tervetuloa',
+      flemish: 'Welgekomen',
+      french: 'Bienvenue',
+      german: 'Willkommen',
+      irish: 'Failte',
+      italian: 'Benvenuto',
+      latvian: 'Gaidits',
+      lithuanian: 'Laukiamas',
+      polish: 'Witamy',
+      spanish: 'Bienvenido',
+      swedish: 'Valkommen',
+      welsh: 'Croeso'
+    }
+    if (translator[language] == null || translator[language] == undefined || 
+        translator[language] == "IP_ADDRESS_INVALID" || 
+        translator[language] == "IP_ADDRESS_NOT_FOUND" || 
+        translator[language] == "IP_ADDRESS_REQUIRED"){
+      return translator.english;
+    } else { 
+      return translator[language];
+      }
+  }
