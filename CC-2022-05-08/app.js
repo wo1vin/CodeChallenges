f95e0bc414d54da82235747872d0a607c2f8191a
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-05-08 14:11:46 -0400

    All Star Code Challenge #18

---
+++ CC-2022-05-08/app.js
@@ -0,0 +1,19 @@
+/* Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned. */
+// function strCount(str, letter){  
+//     let count = 0;
+//     str.split("");
+//     for(let i = 0; i <= str.length; i++){
+//       if(str[i] == letter){
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+
+ // or 
+
+  function strCount(str, letter){  
+    return str.split(letter).length-1
+  }
