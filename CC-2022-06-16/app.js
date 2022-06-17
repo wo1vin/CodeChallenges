3d4dafa56c75a7e9be337ce2cf360ae520ee6247
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-06-16 20:34:20 -0400

    Reverse List Order

--- CC-2022-06-16/app.js
+++ CC-2022-06-16/app.js
@@ -1,4 +1,9 @@
 /* In this kata you will create a function that takes in a list and returns a list with the reverse order. */ 
+
+// function p: list array? r: reversed list 
+//PREP
+//P: array? empty? 
+//R: array
 function reverseList(list) {
     return list.reverse();
   }
