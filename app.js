fb3db093aa2e08576233e199cdf5fdaccfc445bd
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-06-19 08:57:38 -0400

    Hello, Name or World

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-/* In this kata you will create a function that takes in a list and returns a list with the reverse order. */ 
-function reverseList(list) {
-    return list.reverse();
+/* Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx). */
+function hello(name) {
+    //PREP
+    //P: capitalized name string no typos, no other types
+    //R: specified string based on input
+    //E: Bob -> "Hello, Bob!"
+    //P: passing in a name, fix any typos in the capitalization and
+    // check if name is empty to return "Hello, World!" otherwise return "Hello, Xxxx"\
+    if(name){
+      return `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
+    } else {
+      return 'Hello, World!';
   }
+    }
